04cbb8cc9b9292625ca3eca45fa7525ee5737114
Lwimix <martilwinx@gmail.com>
2023-08-11 22:50:05 +0300

    New 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-def my_addition():
-    """adding two numbers"""
-    a = 1
-    b = 2
-    from add_0 import add
-    print(str(a), "+", str(b), "=", str(add(a, b)))
-
-
+"""def my_addition():
+   adding two numbers"""
+from add_0 import add
+a = 1
+b = 2
 if __name__ == "__main__":
-    my_addition()
+    print(str(a), "+", str(b), "=", str(add(a, b)))
