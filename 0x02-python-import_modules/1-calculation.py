5eeeee3c0b6a9898ff7a39cc4dbed42dc1f4cc0e
Lwimix <martilwinx@gmail.com>
2023-08-11 23:30:57 +0300

    Different 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-def calculator():
-    """four calculations at once"""
-    a = 10
-    b = 5
-    import calculator_1 as calcs
+import calculator_1 as calcs
+a = 10
+b = 5
+if __name__ == "__main__":
     print(str(a), "+", str(b), "=", str(calcs.add(a, b)))
     print(str(a), "-", str(b), "=", str(calcs.sub(a, b)))
     print(str(a), "*", str(b), "=", str(calcs.mul(a, b)))
     print(str(a), "/", str(b), "=", str(calcs.div(a, b)))
-
-
-if __name__ == "__main__":
-    calculator()
