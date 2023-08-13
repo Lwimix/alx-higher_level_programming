70fade3aefe9e459be8fd7618f615d8513ad3a98
Lwimix <martilwinx@gmail.com>
2023-08-13 22:17:44 +0300

    0 is a number too

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    counter = 0
-    len_my_list = len(my_list)
-    while counter < len_my_list:
-        print("{}".format(my_list[counter]))
-        counter = counter + 1
+    for item in my_list:
+        print("{}".format(item))
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     no_of_elements = len(my_list)
     cpy_list = my_list
-    if idx < 0 | idx > no_of_elements:
+    if idx < 0 or idx > no_of_elements:
         return my_list
     else:
         cpy_list[idx] = element
