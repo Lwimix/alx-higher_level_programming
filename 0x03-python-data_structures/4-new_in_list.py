28b0fe5b5871aaf61de392c0d8b0e6b1f7ed3a4a
Lwimix <martilwinx@gmail.com>
2023-08-14 08:05:56 +0300

    Index exceeding

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     cpy_list = my_list[:]
     new_cpy_list = my_list
     len_list = len(my_list)
-    if idx < 0 or idx > len_list:
+    if idx < 0 or idx >= len_list:
         return new_cpy_list
     else:
         cpy_list[idx] = element
