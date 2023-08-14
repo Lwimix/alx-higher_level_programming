bd1733aecfe63d74e930a8c1465de4b5920aa3b3
Lwimix <martilwinx@gmail.com>
2023-08-14 22:16:05 +0300

    Change 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     counter = 0
     for item in matrix:
-        print(" ".join(format(item) for item in matrix[counter]))
+        print(*matrix[counter])
         counter = counter + 1
