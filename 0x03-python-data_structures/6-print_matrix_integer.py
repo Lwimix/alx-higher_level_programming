a9e2709718497caa4bda92e6a3a90a5a072cddd3
Lwimix <martilwinx@gmail.com>
2023-08-15 19:31:16 +0300

    The Matrix 2

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     counter = 0
-    for item in matrix:
-        print(" ".join(format(n) for n in matrix[counter]))
+    count = 0
+    len_outer_matrix = len(matrix)
+    while counter < len_outer_matrix:
+        len_matrix = len(matrix[counter])
+        while count <  len_matrix:
+            if count + 1 == len_matrix:
+                print("{:d}".format(matrix[counter][count]))
+                count = count + 1
+            else:
+                print("{:d}".format(matrix[counter][count]), end = " ")
+                count = count + 1
+        count = 0
         counter = counter + 1
+    print("")
