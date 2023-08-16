f8a27e968b6bb6e5b4fc0425f2a70d5c7f0af338
Lwimix <martilwinx@gmail.com>
2023-08-16 19:00:06 +0300

    The Seven

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or not len(roman_string):
+        return None
+    if roman_string[0] is "I":
+    if roman_string[0] is "X":
+    if roman_string[0] is "C":
+    if roman_string[0] is "M":
--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    keys = list(a_dictionary.keys())
-    cpy_a_dict = a_dictionary.copy()
-    for item in keys:
-        if item == key:
-            a_dictionary[key] = value
-            return cpy_a_dict
-    cpy_a = list(a_dictionary.items())
-    new_value = (key, value)
-    cpy_a.append(new_value)
-    cpy_a = dict(cpy_a)
-    return cpy_a
+    if key in a_dictionary:
+        a_dictionary[key] = value
+        return a_dictionary
+    else:
+        a_dictionary.update({key: value})
+        return a_dictionary
