a86de8af2b8412f59055ace1eeb65626f2b5c585
Lwimix <martilwinx@gmail.com>
2023-09-11 22:06:39 +0300

    No 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -11,19 +11,6 @@ class MyList(list):
         """Print_sorted method
         Prints a sorted list
         """
-        sorting = self
-        smallest = sorting[0]
-        ind = 0
-        my_sort = []
-        i = 0
-        for item in sorting:
-            while (i < len(sorting)):
-                if sorting[i] <= smallest:
-                    smallest = sorting[i]
-                i = i + 1
-            my_sort.append(smallest)
-            ind = ind + 1
-            if ind < len(sorting):
-                smallest = sorting[ind]
-            i = ind
-        return my_sort
+        sorting = self[:]
+        sorting.sort()
+        print(sorting)
