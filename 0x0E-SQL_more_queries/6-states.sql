24be755dc4352df884752532142255138934c7a9
Lwimix <martilwinx@gmail.com>
2023-10-18 12:29:26 -0700

    Corrections

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- this script creates the database hbtn_0d_usa
 -- with a table states on my MYSQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
