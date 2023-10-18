7fa2e9043467af67c0c6b93993c90de36b988d86
Lwimix <martilwinx@gmail.com>
2023-10-18 13:22:09 -0700

    Final ones

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- this script lista all the shows contained in
+-- hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM hbtn_0d_tvshows AS tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
