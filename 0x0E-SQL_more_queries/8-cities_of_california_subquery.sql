4d7a856a252d7687df7a13400a9a95893e58a7ac
Lwimix <martilwinx@gmail.com>
2023-10-18 12:45:29 -0700

    8-9

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,10 @@
 -- this script lists all cities of California
 -- found in database hbtn_0d_usa
-SELECT id, name
+SELECT *
+FROM cities
+WHERE state_id = (
+	SELECT id
+	FROM states
+	WHERE name = 'California'
+)
+ORDER BY id
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- this script lists all cities contained in
 -- database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
-FROM cities, states
+FROM cities
+JOIN states ON cities.state_id =  states.id
 ORDER BY cities.id
