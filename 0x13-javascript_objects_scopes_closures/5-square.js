fb43ca9962e0177e3d9dc683a191f4e1921cf875
Lwimix <martilwinx@gmail.com>
2023-11-15 01:54:41 +0300

    A pair of 6

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -7,5 +7,13 @@ class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
+
+  charPrint (c) {
+    let printed = 'X';
+    if (c === undefined) {
+      printed = 'X';
+    } else { printed = c; }
+    for (let i = 0; i < Square.size; i++) { console.log(printed.repeat(Square.size)); }
+  }
 }
 module.exports = Square;
