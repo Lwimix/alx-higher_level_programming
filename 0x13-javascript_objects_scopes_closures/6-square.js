f7d4e7dc5ce09625d3bceb04e4cc6e202a37be4b
Lwimix <martilwinx@gmail.com>
2023-11-15 01:22:28 +0300

    6 is so old

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,11 +3,11 @@
  * This script creates a Square class that inherits from Square
  */
 const Square = require('./5-square');
-  Square.prototype.charPrint = function(c) {
-    let printed = 'X';
-    if (c === undefined) {
-      printed = 'X';
-    } else { printed = c; }
-    for (let i = 0; i < this.size; i++) { console.log(printed.repeat(this.size)); }
-  }
+Square.prototype.charPrint = function (c) {
+  let printed = 'X';
+  if (c === undefined) {
+    printed = 'X';
+  } else { printed = c; }
+  for (let i = 0; i < this.size; i++) { console.log(printed.repeat(this.size)); }
+};
 module.exports = Square;
