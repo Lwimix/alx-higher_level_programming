0117dded85a11c5ed255c54da6fe52e94a267686
Lwimix <martilwinx@gmail.com>
2023-11-15 00:41:14 +0300

    101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+ * This script sorts a dictionary according to id
+ */
+const dict = require('./101-main.js').dict;
+const myDict = {}; const keys = Object.keys(dict);
+for (let i = 0; i < keys.length; i++) {
+  if (myDict[dict[keys[i]]]) { myDict[dict[keys[i]]].push(keys[i]); } else { myDict[dict[keys[i]]] = [].concat(keys[i]); }
+}
+console.log(myDict);
