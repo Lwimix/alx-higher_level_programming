b0de86ab737a2516d96ca673cd4dcaf3aee8b013
Lwimix <martilwinx@gmail.com>
2023-11-15 01:21:12 +0300

    6 is so old

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,13 @@
 #!/usr/bin/node
 /*
- * This script creates a Square class that inherits from Rectangle
+ * This script creates a Square class that inherits from Square
  */
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
-  charPrint (c) {
+const Square = require('./5-square');
+  Square.prototype.charPrint = function(c) {
     let printed = 'X';
     if (c === undefined) {
       printed = 'X';
     } else { printed = c; }
     for (let i = 0; i < this.size; i++) { console.log(printed.repeat(this.size)); }
   }
-}
 module.exports = Square;
