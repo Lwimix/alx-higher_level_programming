4319a985c9303435a899aa3cbb265407eae65bbb
Lwimix <martilwinx@gmail.com>
2023-12-01 00:01:45 +0300

    Requirements

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script sends a GET request and only returns a
-#+response with a 200 status code
+# This script sends a GET request and only returns a response with a 200 code
 grep "200 OK" | curl -sL "$1"
