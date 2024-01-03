72fbd4f233cf3db9d25acf0ead19ca8df02fa0fd
Lwimix <martilwinx@gmail.com>
2024-01-04 02:14:59 +0300

    Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,22 +7,19 @@ req(url, (error, response, body) => {
     return;
   }
   const json = JSON.parse(body);
-  const charcts = json.results[1].characters;
-  let i = 0;
-  let name;
-  while (i < charcts.length) {
-    if (charcts[i].includes('18')) {
-      name = charcts[i];
-      break;
+  const items = json.results;
+  let j = 0;
+  let count = 0;
+  while (j < items.length) {
+    const charcts = json.results[j].characters;
+    let i = 0;
+    while (i < charcts.length) {
+      if (charcts[i].includes('18')) {
+        count++;
+      }
+      i++;
     }
-    i++;
+    j++;
   }
-  req(name, (error, response, body) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
-    const js = JSON.parse(body);
-    console.log(js.films.length);
-  });
+  console.log(count);
 });
